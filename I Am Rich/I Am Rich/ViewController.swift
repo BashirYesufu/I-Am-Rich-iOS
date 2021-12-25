0bf1f9ea371558e7cfa27dd500563a1a9966e3b9
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-25 16:14:22 +0100

    Added all files

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by mac on 12/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
